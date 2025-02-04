a4f524a2a03b8d7fd4d838786bb2061be24260c5
Michael Steven Avila Cortes <michael.avila@gse.com.co>
2025-02-04 10:00:47 -0500

    Primer commit

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routers import tasks
-
-app = FastAPI()
-app.include_router(tasks.router)
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the To-Do List API"}
